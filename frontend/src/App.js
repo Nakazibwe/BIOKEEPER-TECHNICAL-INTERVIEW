97c75f299941f9317c9c7eb34e260a4ecf93659d
Grace Nakazibwe <nakazibwe19grace@gmail.com>
2023-10-09 12:32:29 +0300

    Route protection added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,18 +6,46 @@ import Login from './components/Login.jsx';
 import EditStaff from './components/EditStaff';
 import DeleteStaff from './components/DeleteStaff';
 import Register from './components/Register';
-
+import Guard from './Guard';
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path='/' element={<Hieracy />} />
-        <Route path='/addstaff' element={<AddStaff />} />
+        <Route path='/register' element={<Register />} />
         <Route path='/login' element={<Login />} />
-        <Route path='/staff/:staffid' element={<EditStaff />} />
-        <Route path='/deletestaff/:staffid' element={<DeleteStaff />} />
-        <Route path='/register' element={<Register/>}/>
+        <Route
+          path='/'
+          element={
+            <Guard>
+              <Hieracy />
+            </Guard>
+          }
+        />
+        <Route
+          path='/addstaff'
+          element={
+            <Guard>
+              <AddStaff />
+            </Guard>
+          }
+        />
+        <Route
+          path='/staff/:staffid'
+          element={
+            <Guard>
+              <EditStaff />
+            </Guard>
+          }
+        />
+        <Route
+          path='/deletestaff/:staffid'
+          element={
+            <Guard>
+              <DeleteStaff />
+            </Guard>
+          }
+        />
       </Routes>
     </>
   );
